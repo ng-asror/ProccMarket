b823777be579cf54280dd9052c7b54e12be5645b
Асрор Шодиев <144030637+ng-asror@users.noreply.github.com>
2025-09-06 13:24:17 +0500

    update front

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+bootstrapApplication(App, appConfig)
+  .then(() => {
+    const loader = document.getElementById('app-loader');
+    if (loader) {
+      loader.style.display = 'none';
+    }
+  })
+  .catch((err) => console.error(err));

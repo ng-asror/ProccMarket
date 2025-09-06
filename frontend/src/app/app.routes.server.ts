10b1aa918c752b6f5a29daf673d1297f3432488e
Асрор Шодиев <144030637+ng-asror@users.noreply.github.com>
2025-09-06 12:50:20 +0500

    update app.routes.server.ts

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,40 +1,13 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: 'home',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: 'search',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: 'forum',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: 'profile',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: 'balance',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: 'comments/:id',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: 'section/:id',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender,
-  },
-  {
-    path: '',
-    renderMode: RenderMode.Server,
-  },
+  { path: 'home', renderMode: RenderMode.Server },
+  { path: 'search', renderMode: RenderMode.Server },
+  { path: 'forum', renderMode: RenderMode.Server },
+  { path: 'forum/section/:id', renderMode: RenderMode.Server },
+  { path: 'profile', renderMode: RenderMode.Server },
+  { path: 'balance', renderMode: RenderMode.Server },
+  { path: 'comments/:id', renderMode: RenderMode.Server },
+  { path: '', renderMode: RenderMode.Server },
+  { path: '**', renderMode: RenderMode.Prerender },
 ];

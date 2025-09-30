891174608d87e6c318bf8e7aa2950285981295fd
Асрор Шодиев <144030637+ng-asror@users.noreply.github.com>
2025-09-30 11:41:07 +0500

    sidebar update and home

--- frontend/src/app/modules/home/components/tab-content/tab-content.ts
+++ frontend/src/app/modules/home/components/tab-content/tab-content.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { FormCard } from '../../../../components';
+import { icons, LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-tab-content',
-  imports: [FormCard],
+  imports: [FormCard, LucideAngularModule],
   templateUrl: './tab-content.html',
   styleUrl: './tab-content.scss',
 })
-export class TabContent {}
+export class TabContent {
+  ICONS = icons;
+}

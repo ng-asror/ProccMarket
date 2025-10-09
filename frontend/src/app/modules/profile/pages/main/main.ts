56c923a4152d9c48c0cdf9fb53a1ab1efeee4268
Асрор Шодиев <144030637+ng-asror@users.noreply.github.com>
2025-10-09 17:03:18 +0500

    rewiew yangilandi

--- frontend/src/app/modules/profile/pages/main/main.ts
+++ frontend/src/app/modules/profile/pages/main/main.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, inject, resource, signal } from '@a
 import { Router, RouterLink } from '@angular/router';
 import { icons, LucideAngularModule } from 'lucide-angular';
 import { firstValueFrom } from 'rxjs';
-import { Auth, Layout, NumeralPipe, ProfileService } from '../../../../core';
+import { Auth, Layout, NumeralPipe, ProfileService, Telegram } from '../../../../core';
 import { NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -17,9 +17,10 @@ export class Main {
   private authService = inject(Auth);
   private profileService = inject(ProfileService);
   private layoutService = inject(Layout);
+  private telegram = inject(Telegram);
+
   protected ICONS = icons;
   protected message: string = '';
-  dialog: HTMLDialogElement | null = document.getElementById('my_modal_5') as HTMLDialogElement;
   protected rating: number = 4;
   protected logout(): void {
     this.authService.logout();
@@ -29,9 +30,30 @@ export class Main {
   });
 
   async sendReview(): Promise<void> {
-    this.dialog?.close();
-    await firstValueFrom(this.layoutService.writeReview(this.rating, this.message)).then(
-      (res) => {}
-    );
+    const dialog: HTMLDialogElement | null = document.getElementById(
+      'my_modal_5'
+    ) as HTMLDialogElement;
+    dialog?.close();
+    await firstValueFrom(this.layoutService.writeReview(this.rating, this.message))
+      .then((res) => {
+        this.telegram.showAlert('Отзыв успешно отправлен!');
+        this.message = '';
+        this.rating = 4;
+      })
+      .catch((error) => {
+        if (error.status === 429) {
+          const nextWithdrawal = error.error.next_review_at;
+          const dateObj = new Date(nextWithdrawal);
+          const formatted = dateObj.toLocaleString('ru-RU', {
+            month: 'long',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+          });
+          this.telegram.showAlert(
+            `Вы можете отправлять только один отзыв каждые 24 часа. Следующий отзыв можно будет оставить: ${formatted}`
+          );
+        }
+      });
   }
 }

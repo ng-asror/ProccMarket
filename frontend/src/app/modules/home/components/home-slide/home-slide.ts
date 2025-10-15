b2da31bba679e45c338c428abd8f7631a4749ee6
Асрор Шодиев <144030637+ng-asror@users.noreply.github.com>
2025-10-15 16:39:22 +0500

    update home slide

--- frontend/src/app/modules/home/components/home-slide/home-slide.ts
+++ frontend/src/app/modules/home/components/home-slide/home-slide.ts
@@ -1,22 +1,10 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import SwiperCore from 'swiper';
-import { EffectFade, Grid, Autoplay } from 'swiper/modules';
-
-SwiperCore.use([Grid, Autoplay, EffectFade]);
 
 @Component({
   selector: 'app-home-slide',
+  imports: [],
   templateUrl: './home-slide.html',
-  styleUrls: ['./home-slide.scss'], // styleUrl -> styleUrls
+  styleUrl: './home-slide.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class HomeSlide {
-  slides = [
-    { img: '/img/home-slide1.jpg' },
-    { img: '/img/home-slide2.jpg' },
-    { img: '/img/home-slide3.jpg' },
-    { img: '/img/home-slide1.jpg' },
-    { img: '/img/home-slide2.jpg' },
-    { img: '/img/home-slide3.jpg' },
-  ];
-}
+export class HomeSlide {}
